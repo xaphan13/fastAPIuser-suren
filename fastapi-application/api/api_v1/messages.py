95e528dcdf4a23970b2bdaa36602c8c2e7b3b76d
Max <xaphan@bk.ru>
2026-08-29 00:15:07 +0300

    first commit

---
+++ fastapi-application/api/api_v1/messages.py
@@ -0,0 +1,53 @@
+from typing import Annotated
+
+from fastapi import APIRouter
+from fastapi import Depends
+
+from core.authentication.fastapi_users import (
+    current_active_user,
+    current_active_superuser,
+)
+from core.config import settings
+from core.models import User
+from core.schemas.user import UserRead
+
+router = APIRouter(
+    prefix=settings.api.v1.messages,
+    tags=["Messages"],
+)
+
+
+@router.get("/error")
+def view_may_raise_error(
+    raise_error: bool = False,
+):
+    if raise_error:
+        # 1 / 0
+        UserRead.model_validate(None)
+    return {"ok": True}
+
+
+@router.get("")
+def get_user_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_user),
+    ],
+):
+    return {
+        "messages": ["m1", "m2", "m3"],
+        "user": UserRead.model_validate(user),
+    }
+
+
+@router.get("/secrets")
+def get_superuser_messages(
+    user: Annotated[
+        User,
+        Depends(current_active_superuser),
+    ],
+):
+    return {
+        "messages": ["secret-m1", "secret-m2", "secret-m3"],
+        "user": UserRead.model_validate(user),
+    }
